d7682c60c3645a004808ba04905d01a0d408c50c
Wu-Ke-CUC <wuke0704@outlook.com>
2025-03-19 00:20:06 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,41 @@
-﻿using System;
-namespace Temperature
+using System;
+class Week2
 {
-    class Work8
+    static void Main()
     {
-        static void Main()
+        int input=int.Parse(Console.ReadLine());
+        int[] nums=new int[4];
+        for(int i=0;i<4;i++)
         {
-            double fahrenheit=double.Parse(Console.ReadLine());
-            double centigrade=(fahrenheit-32)/1.8;
-            Console.WriteLine($"摄氏温度是：{centigrade}");
+            nums[i]=input%10;
+            if(nums[i]==0)
+            {
+                nums[i]=10;
+            }
+            input/=10;
         }
+        int p=1;
+        int time=0;
+        for(int i=3;i>=0;i--)
+        {
+            if(p<nums[i])
+            {
+               while(p!=nums[i])
+               {
+                   p++;
+                   time++;
+               }
+            }
+            else
+            {
+                while(p!=nums[i])
+                {
+                    p--;
+                    time++;
+                }
+            }
+            time++;
+        }
+        Console.WriteLine(time);
     }
 }
